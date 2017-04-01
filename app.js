a948e1d178ad20caf9f9aa2ce9da07a36910c888
Georgi Karaboichev <djorkaevhell@abv.bg>
2017-04-01 21:47:30 +0300

    Fix

--- app.js
+++ app.js
@@ -44,14 +44,14 @@ apiRouter
 	})
 	.post('/auth/login', function(req, res, next)  {
 		if (!req.body.username || !req.body.password) {
-			return res.status(400).json({ "error": "You must send the username and the password" });
+			return res.status(404).json({ "error": "You must send the username and the password" });
 		}
 		db['users'].findOne({ username: req.body.username }, function(err, user) {
 			if (!user) {
-				return res.status(401).json({ "error": "The username or password doesn't match" });
+				return res.status(404).json({ "error": "The username or password doesn't match" });
 			}
 			if (!(user.password === req.body.password)) {
-				return res.status(401).json({ "error": "The username or password doesn't match" });
+				return res.status(404).json({ "error": "The username or password doesn't match" });
 			}
 			
 			res.json({
